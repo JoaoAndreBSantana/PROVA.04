32f2676f92008a09af1a618d45fc0be18132aebe
Joãozin <joaoandresantana38@gmail.com>
2024-08-18 14:44:54 -0300

    Update atv07.PROVA.c

--- atv07.PROVA.c
+++ atv07.PROVA.c
@@ -2,11 +2,11 @@
 
 // ATIVIDADE 7
 int main() {
-    int chave; // vari·vel da chave
+    int chave; // vari√°vel da chave
 
     printf("Digite a chave que seja numero positivo: ");
-    if (scanf("%d", &chave) != 1 || chave <= 0) {  // verifica se a chave È um n˙mero positivo
-        printf("A chave deve ser um n˙mero positivo.\n");  // deu erro
+    if (scanf("%d", &chave) != 1 || chave <= 0) {  // verifica se a chave √© um n√∫mero positivo
+        printf("A chave deve ser um n√∫mero positivo.\n");  // deu erro
         return 1;  // retorna 1 de erro
     }
 
@@ -14,17 +14,17 @@ int main() {
     getchar();  // remove o caractere de nova linha
 
     // Solicita o texto 
-    char caractere;  // vari·vel para armazenar cada caractere do texto
+    char caractere;  // vari√°vel para armazenar cada caractere do texto
     printf("Digite o texto a ser criptografado:\n");
 
 
 
-    // LÍ e criptografa o texto caractere por caractere
+  // L√™ e criptografa o texto caractere por caractere
     while ((caractere = getchar()) != '\n') {  // Continua ate encontrar um caractere de nova linha// esta parte fiz com auxilio de comunidades no reddit
-        if (caractere >= 'A' && caractere <= 'Z') {  // letra mai˙scula
-            printf("%c", (caractere - 'A' + chave)+ 'A');
-        } else if (caractere >= 'a' && caractere = 'z') {  // minuscula
-            printf("%c", (caractere - 'a' + chave)+ 'a');
+        if (caractere >= 'A' && caractere <= 'Z') {  // letra mai√∫scula
+            printf("%c", (caractere - 'A' + chave) % 26 + 'A');
+        } else if (caractere >= 'a' && caractere <= 'z') {  // minuscula
+            printf("%c", (caractere - 'a' + chave) % 26 + 'a');
         } else {
             printf("%c", caractere); // esta parte foi puxada do reddit... eu simplesmente n tava conseguindo resolver
         }
